117d5635816c346e9e8aa713424860b9fbe3caec
Madhur Dixit <dmadhur6@gmail.com>
2019-05-02 13:45:47 +0530

    Both servo motor works at the same time when the object is in proximity

--- serv/serv.ino
+++ serv/serv.ino
@@ -1,14 +1,20 @@
 
 #include <Servo.h>
 
+
+int const trigPin = 11;
+int const echoPin = 12;
 Servo servo1;  // create servo object to control a servo
 Servo servo2;
 // twelve servo objects can be created on most boards
 
 int pos1 = 0;    
-int pos2=0;// variable to store the servo position
+
 
 void setup() {
+
+   pinMode(trigPin, OUTPUT); // trig pin will have pulses output
+  pinMode(echoPin, INPUT); // echo pin should be input to get pulse width
   servo1.attach(9);
   servo2.attach(7);
   
@@ -16,7 +22,27 @@ void setup() {
 }
 
 void loop() {
-  mov1();
+
+
+  int duration, distance;
+  // Output pulse with 1ms width on trigPin
+  digitalWrite(trigPin, HIGH); 
+  delay(1);
+  digitalWrite(trigPin, LOW);
+  // Measure the pulse input in echo pin
+  duration = pulseIn(echoPin, HIGH);
+  // Distance is half the duration devided by 29.1 (from datasheet)
+  distance = (duration/2) / 29.1;
+  // if distance less than 0.5 meter and more than 0 (0 or less means over range) 
+    if (distance <= 50 && distance >= 0) {
+      // LED on
+      mov1();
+    } else {
+      mov2();
+      
+    }
+    delay(60);
+  
  
 }
 
@@ -36,17 +62,10 @@ void mov1()
   
 }
 
-
 void mov2()
 {
-  for (pos2 = 0; pos2 <= 180; pos2 += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    servo2.write(pos2);              // tell servo to go to position in variable 'pos'
+    servo1.write(0);
+    servo2.write(0);// tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos2 = 180; pos2 >= 0; pos2 -= 1) { // goes from 180 degrees to 0 degrees
-    servo2.write(pos2);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
   
 }
